5c9dc80a160f42592c99e986222a14d4982b4156
Nicolas Venzmer <nicolas.venzmer@rga.com>
2023-08-02 09:52:33 -0300

    feat(app): finishing portfolio sections

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
-import "./testimonials.css"
+import React from "react";
+import "./testimonials.css";
+import Avatar1 from "../../assets/avatar1.jpg";
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const data = [
+  {
+    avatar: Avatar1,
+    name: "Test 1",
+    review: "This is a review"
+  },
+  {
+    avatar: Avatar1,
+    name: "Test 1",
+    review: "This is a review"
+  },
+  {
+    avatar: Avatar1,
+    name: "Test 1",
+    review: "This is a review"
+  },
+  {
+    avatar: Avatar1,
+    name: "Test 1",
+    review: "This is a review"
+  },
+  {
+    avatar: Avatar1,
+    name: "Test 1",
+    review: "This is a review"
+  },
+  {
+    avatar: Avatar1,
+    name: "Test 1",
+    review: "This is a review"
+  }
+]
 
 const Testimonials = () => {
   return (
     <section id="testimonials">
-      Testimonials
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials__container">
+      {data.map(({avatar, name, review}, index) =>{
+        return (
+        <SwiperSlide key={index} className="testimonial">
+          <div className="client__avatar">
+            <img src={avatar} alt="Avatar One" />
+          </div>
+          <h5 className="client__name">{name}</h5>
+          <small className="client__review">{review}</small>
+        </SwiperSlide>
+        )
+      })}
+      </Swiper>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
